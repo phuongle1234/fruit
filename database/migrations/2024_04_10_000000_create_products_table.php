b2075d1dac5609418b5a02d56cc500e1e83f9941
DavidPhuong98 <105845636+DavidPhuong98@users.noreply.github.com>
2024-04-10 16:51:18 +0700

    setup soucre

---
+++ database/migrations/2024_04_10_000000_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('category_id');
+            $table->tinyInteger('unit');
+            $table->tinyInteger('price');
+            $table->tinyInteger('quantity');
+            $table->tinyInteger('amount');
+            $table->timestamps();
+
+            // relation ship
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Products');
+    }
+}
